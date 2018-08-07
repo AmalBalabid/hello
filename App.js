ad116628a66c3930e5624b4776f28d43acdab534
amalb3 <41971847+amalb3@users.noreply.github.com>
2018-08-07 06:35:26 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div background="BG.jpg">
+ <center>
+ <br/>
+<h1>Amal Balubaid </h1>
+<h4>
+	Computer Science Student</h4>
+	<i calssName="fa fa-twitter"></i>
+<a href="https://twitter.com/i_aml36">i_aml36</a><br/>
+	<i calssName="fa fa-envelope"></i>
+	<a href="mailto:amalbalubaid3@gmail.com">Email</a><br/>
+<i calssName="fa fa-map-pin"></i>
+  Saudi Arabia Jeddah 
+ <br/><br/>
+<h4>Hello, I'm Amal Balubaid computer science student at KAU, level six. 
+<br/>This my first experience to build a website using HTML.
+<br/>I will share my future projects here. 
+</h4>
+
+<a href="https://drive.google.com/file/d/19HnKMVBNUYXPBP6hDKfv_NWYGolGfC4F/view?usp=sharing" >
+  <button calssName="btn"><i calssName="fa fa-download"></i> Download My CV</button>
+</a>
+</center>
+</div>
+      </div>
+    );
+  }
+}
+
+export default App;
