67f0ae409c7492d9b675fde7bd85b78210cb5ba9
amalb3 <41971847+amalb3@users.noreply.github.com>
2018-08-07 06:37:10 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,13 +6,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div background="BG.jpg">
+ <center>
+ <br/>
+<h1>Amal Balubaid </h1>
+<h4>
+	Computer Science Student</h4>
+	<i calssName="fa fa-twitter"></i>
+<a href="https://twitter.com/i_aml36">i_aml36</a><br/>
+	<i calssName="fa fa-envelope"></i>
+	<a href="mailto:amalbalubaid3@gmail.com">Email</a><br/>
+<i calssName="fa fa-map-pin"></i>
+  Saudi Arabia Jeddah 
+ <br/><br/>
+<h4>Hello, I'm Amal Balubaid computer science student at KAU, level six. 
+<br/>This my first experience to build a website using HTML.
+<br/>I will share my future projects here. 
+</h4>
+
+<a href="https://drive.google.com/file/d/19HnKMVBNUYXPBP6hDKfv_NWYGolGfC4F/view?usp=sharing" >
+  <button calssName="btn"><i calssName="fa fa-download"></i> Download My CV</button>
+</a>
+</center>
+</div>
       </div>
     );
   }
